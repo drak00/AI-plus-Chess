8358003306012ecbce71c4aa22ed92f2a1c01685
itibi charles akoche <itibicharles@gmail.com>
2020-10-30 21:25:24 +0100

    Addition of Queen moves
    Addition of queen moves based on the combination of both rook and bishop moves.

--- engine.py
+++ engine.py
@@ -107,7 +107,27 @@ class Game_state():
 
 
 	def get_queen_moves(self, r, c, moves):
+		"""
+					Calculates all possible queen moves for a given color (light or dark)
+					based on bishop and rook moves.
+
+					input parameter(s):
+					r     --> starting row (int)
+					c     --> starting colum (int)
+					moves --> possible moves container (list)
+
+					return parameter(s):
+					None
+				"""
 		##TODO
+		if self.light_to_move:  # if it's light's turn to move
+			self.get_bishop_moves(r, c, moves)
+			self.get_rook_moves(r, c, moves)
+
+		else: # if it's dark's turn to move
+			self.get_bishop_moves(r, c, moves)
+			self.get_rook_moves(r, c, moves)
+
 		pass
 
 
