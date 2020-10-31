4c1bbe529eae03faf2eeeebb5ec81f2825beb3f3
Bilal Babale <bilalbabale@gmail.com>
2020-10-31 17:53:07 +0100

    feat: Add dark pawn moves

--- engine.py
+++ engine.py
@@ -77,9 +77,18 @@ class Game_state():
 
 			for i in range(len(self.board)):
 				for j in range(len(self.board[i])):
-					if self.board[i][j] == "  " or self.board[i][j][1] == "l": # if square is empty or square has opponent's piece
-						moves.append(Move((r, c), (i, j), (self.board))) # create a move object and append to moves
- 
+					# if square is empty and in front of pawn and it is the pawn's first move
+					if ((i == r+2) and (j == c) ) and (r == 1) and (self.board[i][j] == "  "):
+						moves.append(Move((r, c), (i, j), self.board)) # create a move object and append to list
+					
+					# if square is empty and in front of pawn
+					if ((i == r+1) and (j == c) ) and (self.board[i][j] == "  "):
+						moves.append(Move((r, c), (i, j), self.board)) # create a move object and append to list
+					
+					# if square is diagonal to pawn and has an opponent piece
+					if ((i == r+1) and (j == c+1 or j == c-1) and (self.board[i][j][1] == "l")):
+						moves.append(Move((r, c), (i, j), self.board)) # create a move object and append to list
+
 
 	def get_bishop_moves(self, r, c, moves):
 
