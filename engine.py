64a262b8bf03c680ae374e448eab5de925c99a6c
kabir sani <kabirsani39@gmail.com>
2020-11-01 20:24:04 +0100

    Update King_moves

--- engine.py
+++ engine.py
@@ -89,39 +89,7 @@ class Game_state():
 		"""
 
 		##TODO
-		direction = ((-1,-1), (1,1), (1,-1), (-1,1)) # possible  Bishop direction
-		if self.light_to_move: # light piece turn to move
-			for d in direction:
-				for i in range(1,len(self.board)):
-					rownum = r + d[0] * i
-					colnum = c + d[1] * i
-					if (0 <= rownum < len(self.board)) and (0 <= colnum < len(self.board)): # making sure r and c on board
-						if self.board[rownum][colnum] == "  ": # if square is empty
-							moves.append(Move((r,c), (rownum,colnum), (self.board)))
-						elif self.board[rownum][colnum][1] == "d": # if square has opponent piece
-							moves.append(Move((r,c), (rownum,colnum), (self.board)))
-							break
-						else:
-							break # when ally piece encountered
-					else:
-						break # when off the board
-
-
-		else: # Dark piece turn to move
-			for d in direction:
-				for i in range(1,len(self.board)):
-					rownum = r + d[0] * i
-					colnum = c + d[1] * i
-					if (0 <= rownum < len(self.board)) and (0 <= colnum < len(self.board)): # making sure r and c on board
-						if self.board[rownum][colnum] == "  ": # if square is empty
-							moves.append(Move((r,c), (rownum,colnum), (self.board)))
-						elif self.board[rownum][colnum][1] == "l": # if square has opponent piece
-							moves.append(Move((r,c), (rownum,colnum), (self.board)))
-							break
-						else:
-							break # when ally piece encountered
-					else:
-						break # when off the board
+		pass
 
 	def get_knight_moves(self, r, c, moves):
  		##TODO
@@ -141,26 +109,19 @@ class Game_state():
 			return parameter(s):
 			None
  		"""
- 		##TODO
-		rows = (r-1, r, r+1)
-		columns = (c-1, c, c+1)
-		if self.light_to_move: # if it's light's turn to move
-			available_squares = (" ", "d") #if square is empty or has a dark piece
-			for i in range(len(self.board)):
-				for j in range(len(self.board[i])):
-					#if square in any direction is empty or has an opponent, move to square or move to capture
-					if (i in rows) and (j in columns) and (self.board[i][j][1] in available_squares):
-						moves.append(Move((r, c), (i, j), self.board)) # create a move object and append to list 
-		
-		else: #dark turn to move
-			available_squares = (" ", "l")  #if square is empty or has a light piece
-			for i in range(len(self.board)):
-				for j in range(len(self.board[i])):
-					#if square in any direction is empty or has an opponent, move to square or move to capture
-					if (i in rows) and (j in columns) and (self.board[i][j][1] in available_squares):
-						moves.append(Move((r, c), (i, j), self.board)) # create a move object and append to list 
-    
-
+		##TODO
+		king_directions = ((-1, -1), (-1, 0),(-1, 1),(0, -1),(0, 1),(1, -1),(1, 0),(1, 1)) # all the possible directions a king piece can move
+		if self.light_to_move: # light piece's turn to move
+			friend_piece ="l" 
+		else: #dark piece's turn to move
+			friend_piece = "d"
+		for i in range(8):
+			row = r + king_directions[i][0]
+			column = c +  king_directions[i][1]
+			if 0<= row < 8 and 0 <= column < 8:
+				last_piece = self.board[row][column]
+				if last_piece[1] != friend_piece: 
+					moves.append(Move((r,c), (row, column), self.board))
 
 	def get_rook_moves(self, r, c, moves):
  		##TODO
