ad10ed53df7c3c44c888f1682f45565fd907c758
itibi charles akoche <itibicharles@gmail.com>
2020-11-02 02:14:23 +0100

    Add bishop moves
    Adds the diagonal moves of bishop for both light and dark teams

--- engine.py
+++ engine.py
@@ -104,6 +104,39 @@ class Game_state():
             return parameter(s):
             None
         """
+        direction = ((-1, -1), (1, 1), (1, -1), (-1, 1))  # possible  Bishop direction
+        if self.light_to_move:  # light piece turn to move
+            for d in direction:
+                for i in range(1, len(self.board)):
+                    rownum = r + d[0] * i
+                    colnum = c + d[1] * i
+                    if (0 <= rownum < len(self.board)) and (
+                            0 <= colnum < len(self.board)):  # making sure r and c on board
+                        if self.board[rownum][colnum] == "  ":  # if square is empty
+                            moves.append(Move((r, c), (rownum, colnum), (self.board)))
+                        elif self.board[rownum][colnum][1] == "d":  # if square has opponent piece
+                            moves.append(Move((r, c), (rownum, colnum), (self.board)))
+                            break
+                        else:
+                            break  # when ally piece encountered
+                    else:
+                        break  # when off the board
+        else:  # Dark piece turn to move
+            for d in direction:
+                for i in range(1, len(self.board)):
+                    rownum = r + d[0] * i
+                    colnum = c + d[1] * i
+                    if (0 <= rownum < len(self.board)) and (
+                            0 <= colnum < len(self.board)):  # making sure r and c on board
+                        if self.board[rownum][colnum] == "  ":  # if square is empty
+                            moves.append(Move((r, c), (rownum, colnum), (self.board)))
+                        elif self.board[rownum][colnum][1] == "l":  # if square has opponent piece
+                            moves.append(Move((r, c), (rownum, colnum), (self.board)))
+                            break
+                        else:
+                            break  # when ally piece encountered
+                    else:
+                        break  # when off the board
 
         pass
 
