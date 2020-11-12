112fe467e476735d717ae8e5b5b0fed9da004bb3
Bilal Babale <bilalbabale@gmail.com>
2020-11-12 16:19:06 +0100

    refactor: Make changes for readability and good presentation

--- engine.py
+++ engine.py
@@ -310,17 +310,18 @@ class Game_state():
         self.board[move.start_row][move.start_col] = "  "
         self.board[move.end_row][move.end_col] = move.piece_moved
         self.move_log.append(move) # log move
-        self.light_to_move = not self.light_to_move # next player to move
 
         # Pawn Promotion
         if (move.piece_moved == "pl" and move.end_row == 0) or (move.piece_moved == "pd" and move.end_row == 7):
-            promoted_piece = input("Input q(Queen),r(rook),b(bishop) or n(knight) to promote ") #we can add this to the ui later
+            promote_to = input("Pawn Promotion:\nInput q(Queen), r(rook), b(bishop), or n(knight) to promote: ") #we can add this to the ui later
             promotion_options = ("q","r","b","n")
-            if promoted_piece in promotion_options:
-                self.board[move.end_row][move.end_col] = promoted_piece + move.piece_moved[1]
-                #creates a default queen promotion if wrong input is given
-            else:
+            if promote_to in promotion_options:
+                self.board[move.end_row][move.end_col] = promote_to + move.piece_moved[1]
+            else: #create a default queen promotion if wrong input is given
                 self.board[move.end_row][move.end_col] = "q" + move.piece_moved[1]
+        
+        self.light_to_move = not self.light_to_move # next player to move
+
 
     def undo_move(self, look_ahead_mode = False):
         """
